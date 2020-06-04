9bd73384485cf0e88237b1a2f028d00302c16d63
DyogoBendo <dyogoromagnabendo@gmail.com>
2020-06-04 10:09:59 -0300

    Conceitos primarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+// JSX: Sintaxe de XML em JavaScript
+
+import Header from "./Header";
+
+// Usamos exatamente como se fosse uma tag HTML, colocando <Header/>
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const [counter, setCounter] = useState(0); // retorna um array -> [valor do estado, funcao para atualizar valor do estado]
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <Header title={`Contador: ${counter}`} />
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>
+        Aumentar
+      </button>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; // digo que estou usando para a web
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.render( // Pedindo para renderizar algo na tela, nesse caso, o App
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // E será renderizado dentro dos elemento que possui o id root
 );
